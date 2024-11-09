7c8384637ece02b2927bd8095e45509e3789401b
XicoBr <164006890+XicoBr@users.noreply.github.com>
2024-11-09 17:09:33 -0300

    header melhorado
    - melhorias visuais no header.php

--- projeto-facul_TESTE/header.php
+++ projeto-facul_TESTE/header.php
@@ -6,12 +6,30 @@
     <title>Biblioteca - Sistema de Reserva de Livros</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet">
+    <style>
+         .logo {
+            font-size: 2.5rem;
+            color: #0d6efd;
+            margin-bottom: 1rem;
+        }
+    </style>
 </head>
 <body>
 
     <div class="container my-5">
-        <h1 class="text-center mb-4">LOCALIVRO</h1>
+        <div class="card">
+            <div class="card-body p-4">
+                <!-- Logo e Título -->
+                <div class="text-center mb-4">
+                    <div class="logo">
+                        <i class="bi bi-book"></i>
+                    </div>
+                    <h2 class="mb-3">LOCALIVRO</h2>
+                </div>        
         <div class="text-center mb-4">
+            
             <a href="novo_usuario.php" class="btn btn-primary mr-2">Novo Usuário</a>
             <a href="novo_livro.php" class="btn btn-primary mr-2">Novo Livro</a>
             <a href="historico_emprestimos.php" class="btn btn-primary mr-2">Histórico de Empréstimos</a>
